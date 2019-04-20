f15f533e6e94a5cf5af8e336f9a99988b360e371
Pablo Weisbek <pablo.weisbek@gmail.com>
2019-04-20 09:14:57 -0300

    backend connects to db

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
 
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'playero01',
+    database: 'iseeyou-db'
+  }
+});
 
 const app = express();
 
@@ -10,33 +21,6 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'apples',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'john@gmail.com'
-    }
-  ]
-}
 
 // ROOT Route
 app.get('/', (req, res) => {
@@ -45,61 +29,81 @@ app.get('/', (req, res) => {
 
 // SIGNIN Route
 app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password) {
-      res.json(database.users[0]);
-    } else {
-      res.status(400).json('Invalid username or password');
-    }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('Unable to get user'))
+        } else {
+          res.status(400).json('Invalid credentials')
+        }
+      })
+      .catch(err => res.status(400).json('Invalid credentials'))
 })
 
 // REGISTER Route
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  bcrypt.hash(password, null, null, function(err, hash) {
-    console.log(hash)
-  });
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length-1])
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+      })
+      .catch(err => res.status(400).json('Unable to register'))
 })
 
 // PROFILE Route
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id ) {
-      found = true;
-      return res.json(user);
-    }
+  db.select('*').from('users').where({
+    id: id
   })
-  if (!found) {
-    res.status(400).json('User not found');
-  }
+  .then(user => {
+    user.length ? res.json(user[0]) : res.status(400).json('Not found...')
+  })
+  .catch(err => res.status(400).json('error getting user'))
+//  if (!found) {
+//    res.status(400).json('User not found');
+//  }
 })
 
 // RANK Route
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id ) {
-      found = true;
-      user.entries++
-      return res.json(user.entries);
-    }
+  db('users')
+  .where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
   })
-  if (!found) {
-    res.status(400).json('User not found');
-  }
+  .catch(err => res.status(400).json('unable to get entries'))
 })
 
 
@@ -112,8 +116,9 @@ app.put('/image', (req, res) => {
 //     // res = false
 // });
 
-app.listen(3001, () => {
-  console.log('App is live on port 3001');
+const port = 3001;
+app.listen(port, () => {
+  console.log('App is live on port ' + port);
 })
 
 
